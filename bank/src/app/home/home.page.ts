005b0dac425580faa3dee4631689b0a4822fc335
Akhilesh Pothuri <pothuriakhilesh@yahoo.com>
2019-11-28 23:38:19 +0530

    HTTP module added

---
+++ bank/src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  ifsc:string;
+  constructor(private http: HttpClient) {
+    this.ifsc=""
+  }
+
+}
