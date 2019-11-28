2a4851f4c837f166e4c4089e0ff2b489b3d42e0a
Akhilesh Pothuri <pothuriakhilesh@yahoo.com>
2019-11-29 00:25:54 +0530

    passing an area variable worked

--- bank/src/app/home/home.page.ts
+++ bank/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,8 +8,26 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
   ifsc:string;
+  area:string;
   constructor(private http: HttpClient) {
-    this.ifsc=""
+    this.ifsc="";
   }
-
+  ionViewWillEnter() {
+    // Load the data
+this.area="MUMBAI";
+    this.prepareDataRequest(this.area)
+      .subscribe(
+        data => {
+          // Set the data to display in the template
+          this.ifsc = JSON.stringify(data);
+          console.log(this.ifsc);
+        }
+      );
+  }
+  private prepareDataRequest(areas:string): Observable<object> {
+    // Define the data URL
+    const dataUrl = 'https://vast-shore-74260.herokuapp.com/banks?city='+areas;
+    // Prepare the request
+    return this.http.get(dataUrl);
+  }
 }
