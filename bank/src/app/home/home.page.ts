441e185e49ee2c304af20d1b22d5924c3656bac1
Akhilesh Pothuri <pothuriakhilesh@yahoo.com>
2019-11-29 09:30:30 +0530

    Search bar functionality implemented

--- bank/src/app/home/home.page.ts
+++ bank/src/app/home/home.page.ts
@@ -8,15 +8,18 @@ import { Observable } from 'rxjs';
 })
 export class HomePage {
   ifsc:string;
+  searchTerm: string ;
+  filteritems:any;
   place: any;
  public banks:any;
+  
   constructor(private http: HttpClient) {
     this.ifsc="";
  
   }
   Retrievedata() {
     // Load the data
-    
+   
 let area=this.place;
 if(area!=null)
     {this.prepareDataRequest(area)
@@ -24,10 +27,18 @@ if(area!=null)
         data => {
           // Set the data to display in the template
           this.banks = data;
+          
           console.log(this.banks);
+          this.filterItems= this.banks;
+         
         }
       );
   }}
+  filterItems(ev:any) {
+    let val = ev.target.value;
+
+    this.filteritems = this.banks.filter(bank =>  bank.bank_name.toLowerCase().indexOf(val.toLowerCase()) > -1 
+    );}
   private prepareDataRequest(areas:string): Observable<object> {
     // Define the data URL
     const dataUrl = 'https://vast-shore-74260.herokuapp.com/banks?city='+areas;
